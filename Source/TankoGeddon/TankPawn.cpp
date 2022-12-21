dee5107b778f392d4c8f8c8048b07014cc87005d
Razdoburdina <razdoburdina.nadya@gmail.com>
2022-12-21 23:58:46 +0300

    make MoveRight, MoveForward

---
+++ Source/TankoGeddon/TankPawn.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPawn.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+ATankPawn::ATankPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxCollision = CreateDefaultSubobject <UBoxComponent>(TEXT("BoxCollision"));
+	RootComponent = BoxCollision;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	BodyMesh->SetupAttachment(BoxCollision);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject <USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(BoxCollision);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+
+	Camera = CreateDefaultSubobject <UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+
+}
+
+// Called when the game starts or when spawned
+void ATankPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATankPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector currentLocation = GetActorLocation();
+
+	//MoveForward
+	if (ForwardMoveAxisValue != 0)
+	{
+	FVector ForwardVector = GetActorForwardVector();
+	FVector moveForwardPosition = currentLocation + ForwardVector * MovementSpeed * ForwardMoveAxisValue * DeltaTime;
+	SetActorLocation(moveForwardPosition);
+	}
+
+	//MoveRight 
+	if (RightMoveAxisValue != 0)
+	{
+	FVector RightVector = GetActorRightVector();
+	FVector moveRightPosition = currentLocation + RightVector * MovementSpeed * RightMoveAxisValue * DeltaTime;
+	SetActorLocation(moveRightPosition);
+	}
+}
+
+void ATankPawn::MoveForward(float Value)
+{
+	ForwardMoveAxisValue = Value;
+}
+
+void ATankPawn::MoveRight(float Value)
+{
+	RightMoveAxisValue = Value;
+}
+
+
+
